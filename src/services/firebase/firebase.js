328704de8db2c39bc2ba2dc0a97bef61f69e7807
Angel <angelggdev@gmail.com>
2021-10-08 15:41:23 -0300

    creation of .env file to hide sensitive information

--- src/services/firebase/firebase.js
+++ src/services/firebase/firebase.js
@@ -1,14 +1,14 @@
 import * as firebase from 'firebase/app';
 import {getFirestore} from 'firebase/firestore';
-import { collection, getDocs, query, where, getDoc, doc } from 'firebase/firestore'; 
+import { collection, getDocs, query, where, getDoc, doc } from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDkZoKmtxD5C21wK-gK_Y3K46Mk3zVcsBY",
-    authDomain: "geek-shop-4f202.firebaseapp.com",
-    projectId: "geek-shop-4f202",
-    storageBucket: "geek-shop-4f202.appspot.com",
-    messagingSenderId: "897582697618",
-    appId: "1:897582697618:web:a0b3bf26c3336829f2ea4b"
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId
 };
 
 const app = firebase.initializeApp(firebaseConfig);
